5e96c78e5a0b34a229aabacc759012d46eebf9ab
pworontzoff <pierre.worontzoff@hepl.be>
2025-04-04 12:08:08 +0200

    Update main.c

--- Fourmi8/main.c
+++ Fourmi8/main.c
@@ -2,5 +2,17 @@
 
 int main() {
     init_paper(7, 7, 25, 0.5, 0);
-    display_paper();  
+    move_to(3,3);
+
+    repeat (8) {
+        if (is_colored()) {
+            erase();
+            ant_turns_left_then_move();
+        } else {
+            colorize();
+            ant_turns_right_then_move();
+        }
+    } loop;
+
+    display_paper();    
 }
