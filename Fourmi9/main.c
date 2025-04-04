38e513f08011c14d8627e16d07c1ced3994ba955
pworontzoff <pierre.worontzoff@hepl.be>
2025-04-04 12:23:25 +0200

    Update main.c

--- Fourmi9/main.c
+++ Fourmi9/main.c
@@ -1,6 +1,18 @@
 #include "paper.h"
 
 int main() {
-    init_paper(7, 7, 25, 0.5, 0);
-    display_paper();  
+    init_paper(15, 15, 20, 0.025, 0);
+    move_to(7,7);
+
+    repeat (472) {
+        if (is_colored()) {
+            erase();
+            ant_turns_left_then_move();
+        } else {
+            colorize();
+            ant_turns_right_then_move();
+        }
+    } loop;
+
+    display_paper(); 
 }
