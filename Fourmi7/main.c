b292b2711f42ca84569eca344d733dcbab2b03c9
pworontzoff <pierre.worontzoff@hepl.be>
2025-04-04 12:03:12 +0200

    Update main.c

--- Fourmi7/main.c
+++ Fourmi7/main.c
@@ -2,5 +2,15 @@
 
 int main() {
     init_paper(7, 7, 25, 0.5, 0);
+    move_to(3,3);
+
+    repeat (8) {
+        if (is_colored()) {
+            erase();
+        } else {
+            colorize();
+        }
+    } loop;
+
     display_paper();  
 }
